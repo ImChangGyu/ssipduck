2577f36437fff58a898915ac8e2b7b8c1d569a98
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-07-19 17:07:04 +0900

    next/image domain 설정 및 emotion compiler 설정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['s4.anilist.co'],
+  },
+  compiler: {
+    emotion: true,
+  },
 };
 
 module.exports = nextConfig;
