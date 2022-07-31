ca9f7b813f74886f637a2ba100e24dfbae2fcfd7
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-07-31 17:29:15 +0900

    Update Selector value atom 추가

--- src/atoms/Atom.ts
+++ src/atoms/Atom.ts
@@ -1,14 +1,26 @@
 import { atom, WritableAtom } from 'jotai';
 
-export const searchValueAtom = atom('');
+export const searchValueAtom = atom<string>('');
+export const selectorValueAtom = atom<string>('POPULARITY_DESC');
 
 export function updateSearchValue(
   initialValue?: string
 ): WritableAtom<string, string | undefined> {
-  const anAtom = atom(initialValue, (get, set, nextValue?: string) => {
+  const searchValue = atom(initialValue, (get, set, nextValue?: string) => {
     const update = nextValue ?? get(searchValueAtom);
-    set(anAtom, update);
+    set(searchValue, update);
   });
 
-  return anAtom as WritableAtom<string, string | undefined>;
+  return searchValue as WritableAtom<string, string | undefined>;
+}
+
+export function updateSelectorValue(
+  initialValue?: string
+): WritableAtom<string, string | undefined> {
+  const selectorValue = atom(initialValue, (get, set, nextValue?: string) => {
+    const update = nextValue ?? get(selectorValueAtom);
+    set(selectorValue, update);
+  });
+
+  return selectorValue as WritableAtom<string, string | undefined>;
 }
