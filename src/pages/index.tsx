de34b8ab8f7fd73910291393284ff8851a3113a1
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-08-26 09:57:15 +0900

    Update SEO 향상을 위한 description 및 OG 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,11 @@ const Home: NextPage = () => {
     <div>
       <Head>
         <title>씹덕</title>
+        <meta name="description" content="씹덕들을 위한 애니메이션 리스트!" />
+        <meta property="og:title" content="씹덕" />
+        <meta property="op:type" content="website" />
+        <meta property="og:url" content="https://ssipduck.vercel.app/" />
+        <meta property="og:image" content="https://ibb.co/Fm89Dgq" />
       </Head>
       <InputContainer />
       <CardContainer />
