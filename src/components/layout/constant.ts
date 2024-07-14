7cee7977dd983028965c5f8e5a43139ed30e6fcd
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2024-07-14 15:09:26 +0900

    feat: layout에서 사용될 nav list 정의

---
+++ src/components/layout/constant.ts
@@ -0,0 +1,7 @@
+export const NAV_LIST = [
+  { label: 'Popular', to: '/' },
+  { label: 'New', to: '/new' },
+  { label: 'Trend', to: '/trend' },
+  { label: 'Movie', to: '/movie' },
+  { label: 'TV', to: '/tv' },
+];
