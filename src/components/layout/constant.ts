e314557ccc9db6c0bf4f17e877cb1d08a7805369
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2024-07-14 18:16:22 +0900

    modify: Header Nav에서 애니메이션의  View 조건 변경

--- src/components/layout/constant.ts
+++ src/components/layout/constant.ts
@@ -1,7 +1,6 @@
 export const NAV_LIST = [
   { label: 'Popular', to: '/' },
-  { label: 'New', to: '/new' },
   { label: 'Trend', to: '/trend' },
+  { label: 'Upcoming', to: '/upcoming' },
   { label: 'Movie', to: '/movie' },
-  { label: 'TV', to: '/tv' },
 ];
