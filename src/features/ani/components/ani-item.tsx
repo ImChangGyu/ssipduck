0a76278158f23aa697cd64e2c65106eba511916c
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2024-07-26 01:18:56 +0900

    feat: 추후 ani modal에서의 aniItem의 재사용을 위해 따로 컴포넌트로 분리

---
+++ src/features/ani/components/ani-item.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+import { AniItemType } from '~/types/ani';
+import { stripTag } from '~/utils/formatter';
+import FavoriteAni from '~/features/ani/components/favorite-ani';
+
+interface AniItemProps {
+  ani: AniItemType;
+}
+
+export default function AniItem({ ani }: AniItemProps) {
+  return (
+    <div className="w-full h-[360px] flex flex-col gap-4 p-6 bg-white border border-transparent rounded-2xl shadow-item hover:border-primary hover:shadow-hover_shadow_color">
+      <Image
+        src={ani.coverImage.extraLarge}
+        width={0}
+        height={0}
+        sizes="100vw"
+        priority
+        className="w-full min-h-[170px] object-cover rounded-lg"
+        alt="anime-main-image"
+      />
+      <div className="w-full h-full flex flex-col justify-between">
+        <div className="flex flex-col gap-[10px]">
+          <div className="w-full flex justify-between items-center">
+            <div className="w-[90%] text-primary_darken font-bold line-clamp-1">
+              {ani.title.romaji}
+            </div>
+            <FavoriteAni aniId={ani.id} />
+          </div>
+          <div className="text-gray_description text-sm mb-4 line-clamp-3">
+            {stripTag(ani.description)}
+          </div>
+        </div>
+        <div className="w-full flex gap-2 truncate">
+          {ani.genres.map((genre, index) => (
+            <span
+              key={`genre_${index}`}
+              className="w-auto text-primary opacity-80 text-xs text-center"
+            >
+              #{genre}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/features/ani/components/ani-list.tsx
+++ src/features/ani/components/ani-list.tsx
@@ -1,13 +1,11 @@
 'use client';
 
 import { getAniListByPage, useAniList } from '~/features/ani/api/get-ani-list';
-import FavoriteAni from '~/features/ani/components/favorite-ani';
-import Image from 'next/image';
 import { VariableType } from '~/types/ani';
-import { stripTag } from '~/utils/formatter';
 import useInfiniteScroll from '~/hooks/useInfiniteScroll';
 import { ANI_VARIABLES } from '~/features/ani/constants/ani-variable';
 import { startTransition } from 'react';
+import AniItem from '~/features/ani/components/ani-item';
 
 interface AniListProps {
   variableType: VariableType;
@@ -35,43 +33,7 @@ export default function AniList({ variableType, searchKeyword }: AniListProps) {
   return (
     <div className="w-full h-full grid gap-[24px] grid-cols-list mb-[5px] px-[3%] py-[20px] md:px-[5%] justify-items-center">
       {aniList.map((ani, index) => (
-        <div
-          key={`ani-item_${ani.id}_${index}`}
-          className="w-full h-[360px] flex flex-col gap-4 p-6 bg-white border border-transparent rounded-2xl shadow-item hover:border-primary hover:shadow-hover_shadow_color"
-        >
-          <Image
-            src={ani.coverImage.extraLarge}
-            width={0}
-            height={0}
-            sizes="100vw"
-            priority
-            className="w-full min-h-[170px] object-cover rounded-lg"
-            alt="anime-main-image"
-          />
-          <div className="w-full h-full flex flex-col justify-between">
-            <div className="flex flex-col gap-[10px]">
-              <div className="w-full flex justify-between items-center">
-                <div className="w-[90%] text-primary_darken font-bold line-clamp-1">
-                  {ani.title.romaji}
-                </div>
-                <FavoriteAni aniId={ani.id} />
-              </div>
-              <div className="text-gray_description text-sm mb-4 line-clamp-3">
-                {stripTag(ani.description)}
-              </div>
-            </div>
-            <div className="w-full flex gap-2 truncate">
-              {ani.genres.map((genre, index) => (
-                <span
-                  key={`genre_${index}`}
-                  className="w-auto text-primary opacity-80 text-xs text-center"
-                >
-                  #{genre}
-                </span>
-              ))}
-            </div>
-          </div>
-        </div>
+        <AniItem ani={ani} key={`ani-item_${ani.id}_${index}`} />
       ))}
       <div ref={ref} />
     </div>
